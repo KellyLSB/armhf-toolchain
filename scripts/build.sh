df74647e543cc405e0ff7d44318e34628bb28aa6
Kelly Lauren-Summer Becker-Neuding <kbecker@kellybecker.me>
2015-05-21 21:04:05 -0700

    Switch docker to use templates

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,29 +1,75 @@
 #!/bin/bash
 
-echo "Updating Apt Sources"; sleep 1
-sudo apt-get -qq update
-echo "Done."; sleep 1
+function missing()  { ! sudo which $1 &>/dev/null; }
+function nochroot() { ! [[ -f build/$1.tar.bz2 ]]; }
 
-echo "Installing Debootstrap and Realpath"; sleep 1
-sudo apt-get -qq install -y debootstrap realpath
-echo "Done."; sleep 1
+#
+# Install missing dependencies.
+#
+if missing debootstrap && missing realpath; then
+	echo "Updating Apt Sources"; sleep 1
+	sudo apt-get -qq update
+	echo "Done."; sleep 1
 
-echo "Setting Project Path"; sleep 1
+	echo "Installing Debootstrap and Realpath"; sleep 1
+	sudo apt-get -qq install -y debootstrap realpath bzip2 tar
+	echo "Done."; sleep 1
+fi
+
+#
+# Setup project environment.
+#
+echo "Setting Project Environment"; sleep 1
+
+# Project environment variables
+echo "Project Environment:"
 : ${PROJECT_PATH:="$(realpath $(dirname $(dirname $0)))"}
-echo "PROJECT_PATH => $PROJECT_PATH"; sleep 1
-echo "Done"; sleep 1
+echo -e "\tPROJECT_PATH => ${PROJECT_PATH}"
+printf "\n"
 
-echo "Creating Chroot"; sleep 1
+# Chroot environment variables
+echo "Chroot Environment:"
+: ${CHROOT_ARCH:="amd64"}
+echo -e "\tCHROOT_ARCH => ${CHROOT_ARCH}"
 : ${CHROOT_DIST:="jessie"}
+echo -e "\tCHROOT_DIST => ${CHROOT_DIST}"
 : ${CHROOT_PATH:="$(mktemp -d)"}
+echo -e "\tCHROOT_PATH => ${CHROOT_PATH}"
 : ${CHROOT_REPO:="http://httpredir.debian.org/debian"}
-sudo debootstrap $CHROOT_DIST $CHROOT_PATH $CHROOT_REPO
-echo "Done."; sleep 1
+echo -e "\tCHROOT_REPO => ${CHROOT_REPO}"
+printf "\n"
 
-echo "Archiving up Chroot"; sleep 1;
-cd $CHROOT_PATH; mkdir -p $PROJECT_PATH/build
-sudo tar -cjvf $PROJECT_PATH/build/chroot.tar.bz2 *
+echo "Done"; sleep 2
+
+#
+# Create chroot and bundle it up.
+#
+if nochroot $CHROOT_DIST; then
+	echo "Creating Chroot"; sleep 1
+	sudo debootstrap $CHROOT_DIST $CHROOT_PATH $CHROOT_REPO
+	echo "Done."; sleep 1
+
+	echo "Archiving up Chroot"; sleep 1;
+	cd $CHROOT_PATH; mkdir -p $PROJECT_PATH/build
+	sudo tar -cpjvf $PROJECT_PATH/build/${CHROOT_DIST}.tar.bz2 *
+	echo "Done."
+fi
+
+#
+# Generate Dockerfile.
+#
+if ! [[ -f ${PROJECT_PATH}/Dockerfile ]]; then
+	echo "Generating Dockerfile..."; sleep 1
+	${PROJECT_PATH}/scripts/render-tpl.sh \
+		${PROJECT_PATH}/tpls/Dockerfile-${CHROOT_ARCH} \
+		${PROJECT_PATH}/Dockerfile
+	echo "Done."; sleep 1
+fi
+
+#
+# Build docker container
+#
+echo "Buildng debian-${CHROOT_DIST}-${CHROOT_ARCH} container."
+sudo docker build -t debian-${CHROOT_DIST}-${CHROOT_ARCH} ${PROJECT_PATH}
+rm ${PROJECT_PATH}/Dockerfile
 echo "Done."
-
-echo "Buildng debian-$CHROOT_DIST container."
-sudo docker build -t debian-$CHROOT_DIST $PROJECT_PATH
---
+++ scripts/render-tpl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function getVars() {   grep -Eo '\{\{[A-Z_]+\}\}' $1 | sort -u;   }
+function getVal()  { eval "echo \$$(grep -Eo '[A-Z_]+' <<<"$1")"; }
+function sedCmd()  {            echo "-e 's/$1/$2/g'";            }
+
+function parse()   {
+	local sed_cmd=()
+  local var val
+
+	for var in $(getVars $1); do
+		val="$(getVal $var)"
+
+		if [[ -n $val ]]; then
+			sed_cmd+=("$(sedCmd $var $val)")
+		fi
+	done
+
+	if [[ -n $sed_cmd ]]; then
+		#echo "sed ${sed_cmd[@]} $1 > $2"
+		eval "sed ${sed_cmd[@]} $1 > $2"
+	fi
+
+	sed_cmd= ; var= ; val= ;
+  unset sed_cmd var val
+}
+
+parse $1 $2
